458d1cd33b1160c2a0cb17acd82a8177594fcc57
“mut0xE” <210969342+mut0xE@users.noreply.github.com>
2026-03-16 11:28:08 +0530

    Feat: parse builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input_ast = parse_macro_input!(input as DeriveInput);
+    eprintln!("{:#?}", input_ast);
+    TokenStream::new()
 }
