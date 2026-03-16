c273bfb60190be689fc08559c56d8119bf3f0570
“mut0xE” <210969342+mut0xE@users.noreply.github.com>
2026-03-16 16:00:36 +0530

    Feat: call build

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -11,6 +11,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let name = input_ast.ident; // Builder
     let bname = format!("{}Builder", name);
     let bident = Ident::new(&bname, name.span()); // CommandBuilder
+
     let expand = quote! {
         pub struct #bident {
               executable: Option<String>,
@@ -18,7 +19,36 @@ pub fn derive(input: TokenStream) -> TokenStream {
               env: Option<Vec<String>>,
               current_dir: Option<String>,
         }
+        impl #bident{
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                        self.executable = Some(executable);
+                        self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                        self.args = Some(args);
+                        self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                        self.env = Some(env);
+                        self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                        self.current_dir = Some(current_dir);
+                        self
+            }
 
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(
+                    #name
+                    {
+                    executable: self.executable.clone().ok_or("should set executable")?,
+                    env: self.env.clone().ok_or("should set env")?,
+                    args: self.args.clone().ok_or("should set args")?,
+                    current_dir: self.current_dir.clone().ok_or("should set current dir")?,
+                }
+                )
+            }
+        }
         impl #name{
             pub fn builder() -> #bident {
                 #bident {
